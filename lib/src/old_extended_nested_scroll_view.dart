effb8f9c52bdbc09b9c14597627962c532e9610f
balvinderz <balvindersi2@gmail.com>
2021-01-27 10:50:05 +0530

    fix scroll view on master

--- lib/src/old_extended_nested_scroll_view.dart
+++ lib/src/old_extended_nested_scroll_view.dart
@@ -1540,6 +1540,11 @@ class _NestedScrollPosition extends ScrollPosition
     _parent?.detach(this);
     super.dispose();
   }
+
+  @override
+  void pointerScroll(double delta) {
+    // TODO: implement pointerScroll
+  }
 }
 
 enum _NestedBallisticScrollActivityMode { outer, inner, independent }
